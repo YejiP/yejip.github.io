9e3abe139c2ab532bac7833d4fb26ec2c52f4859
YejiP <21500280@handong.edu>
2020-11-13 21:29:27 +0900

    1113

---
+++ web/webInd.js
@@ -0,0 +1,62 @@
+window.addEventListener('load', function(){
+  document.getElementById("subject").innerHTML="&nbsp;&nbsp;&nbsp;ALL";
+  jsMain(); });
+
+window.onpageshow = function(event) {
+
+   if ( event.persisted || (window.performance && window.performance.navigation.type == 2)) {
+       jsMain();
+
+   }
+
+};
+
+
+
+function jsMain(){
+document.getElementById("html").style.display="block";
+document.getElementById("js").style.display="block";
+document.getElementById("java").style.display="block";
+document.getElementById("db").style.display="block";
+document.getElementById("subject").innerHTML="&nbsp;&nbsp;&nbsp;ALL";
+var checked = document.getElementsByClassName("title")
+for( var j =0;j<checked.length; j++){
+checked[j].onmouseover= function(){
+if(this.innerHTML=="ALL"){
+document.getElementById("html").style.display="block";
+document.getElementById("js").style.display="block";
+document.getElementById("java").style.display="block";
+document.getElementById("db").style.display="block";
+document.getElementById("subject").innerHTML="&nbsp;&nbsp;&nbsp;ALL";
+
+}
+if(this.innerHTML=="JAVASCRIPT"){
+document.getElementById("java").style.display="none";
+document.getElementById("html").style.display="none";
+document.getElementById("db").style.display="none";
+document.getElementById("js").style.display="block";
+document.getElementById("subject").innerHTML="&nbsp;&nbsp;&nbsp;JAVASCRIPT";
+}
+if(this.innerHTML=="JAVA"){
+document.getElementById("java").style.display="block";
+document.getElementById("html").style.display="none";
+document.getElementById("db").style.display="none";
+document.getElementById("js").style.display="none";
+document.getElementById("subject").innerHTML="&nbsp;&nbsp;&nbsp;JAVA";
+}
+if(this.innerHTML=="DB"){
+document.getElementById("java").style.display="none";
+document.getElementById("html").style.display="none";
+document.getElementById("db").style.display="block";
+document.getElementById("js").style.display="none";
+document.getElementById("subject").innerHTML="&nbsp;&nbsp;&nbsp;DB";
+}
+if(this.innerHTML=="HTML&amp;CSS"){
+document.getElementById("java").style.display="none";
+document.getElementById("html").style.display="block";
+document.getElementById("db").style.display="none";
+document.getElementById("js").style.display="none";
+document.getElementById("subject").innerHTML="&nbsp;&nbsp;&nbsp;HTML&CSS";}
+}
+}
+}
